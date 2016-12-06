bae3ae2d8b42d567ca9aacaf0ac78c73d0493ebb
tmontdev <the.monteiro.dev@gmail.com>
2016-12-06 00:15:49 -0200

    first commit, see you soon

---
+++ assets/js/general-front.js
@@ -0,0 +1,26 @@
+$(function(){
+  //code for mobile and tablet
+  if ($(window).width()<992) {
+    //when an item of menu is clicked
+    $('.menu-item').click(function(event) {
+      //declaring his target
+      target =$(this).attr('data-target');
+      //his target receives a class "shown"
+      $(target).addClass('shown');
+      $('.rollback-area').addClass('dark');
+    });// end of  menu item code
+    //when any rollback is clicked
+    $('.rollback').click(function(event) {
+      $('.app-menu-screen').each(function(index, el) {
+        $(this).removeClass('shown');
+      });
+      $('.rollback-area').removeClass('dark');
+    });
+
+  }//end of  mobile and tablet code
+
+  //code for desktop
+  else{
+
+  }
+})
